13620ebd322e16e31a85677c43cfa36c03a42765
wangbowei <498434962@qq.com>
2020-01-18 12:00:06 +0800

    增加HTTP访问

--- src/main/java/org/jeecg/JeecgApplication.java
+++ src/main/java/org/jeecg/JeecgApplication.java
@@ -4,10 +4,12 @@ import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +20,13 @@ import javax.servlet.MultipartConfigElement;
 @EnableSwagger2
 @EnableScheduling
 public class JeecgApplication {
+
+    @Value("${server.http.port}")
+    private int httpPort;
+
+    @Value("${server.port}")
+    private Integer httpsPort;
+
 
     public static void main(String[] args) {
     	System.setProperty("spring.devtools.restart.enabled", "false");
@@ -31,6 +40,7 @@ public class JeecgApplication {
         return factory.createMultipartConfig();
     }
 
+
     /*@Bean
     MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
@@ -43,17 +53,17 @@ public class JeecgApplication {
      * http重定向到https
      * @return
      */
-   /* @Bean
+    @Bean
     public TomcatServletWebServerFactory servletContainer() {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
             @Override
             protected void postProcessContext(Context context) {
-                SecurityConstraint constraint = new SecurityConstraint();
+               /* SecurityConstraint constraint = new SecurityConstraint();
                 constraint.setUserConstraint("CONFIDENTIAL");
                 SecurityCollection collection = new SecurityCollection();
                 collection.addPattern("/*");
                 constraint.addCollection(collection);
-                context.addConstraint(constraint);
+                context.addConstraint(constraint);*/
             }
         };
         tomcat.addAdditionalTomcatConnectors(httpConnector());
@@ -65,12 +75,12 @@ public class JeecgApplication {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
         //Connector监听的http的端口号
-        connector.setPort(8080);
+        connector.setPort(8081);
         connector.setSecure(false);
         //监听到http的端口号后转向到的https的端口号
-        connector.setRedirectPort(443);
+        connector.setRedirectPort(8080);
         return connector;
-    }*/
+    }
 
 
 
